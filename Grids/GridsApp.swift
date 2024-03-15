a8268c224383e707fe922b3dde076eb6101ee988
sirx2713 <bradx2713@gmail.com>
2024-03-15 15:41:49 +0300

    Initial Commit

---
+++ Grids/GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridsApp.swift
+//  Grids
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/15.
+//
+
+import SwiftUI
+
+@main
+struct GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
